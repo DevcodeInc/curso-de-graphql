0929e136517ca26325cbcf7fc6a17bb0f20e6482
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-11-29 15:24:13 -0400

    create link

---
+++ schema/entities/Link.js
@@ -0,0 +1,10 @@
+const LinkDef = `
+# Esto representa un link
+type Link {
+  id: ID!
+  url: String!
+  description: String!
+}
+`;
+
+module.exports = LinkDef;
--- schema/index.js
+++ schema/index.js
@@ -1,12 +1,14 @@
 const { makeExecutableSchema } = require('graphql-tools');
 
+const LinkDef = require('./entities/Link');
+
 const rootQuery = `
 type Query {
-  allWords: [String]
+  allLinks: [Link!]
 }
 `;
 
 // Generate the schema object from your types definition.
 module.exports = makeExecutableSchema({
-  typeDefs: [rootQuery],
+  typeDefs: [rootQuery, LinkDef],
 });
